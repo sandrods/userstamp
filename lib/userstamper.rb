be8c44fc7ef248f3bad162ca98e695336fe59b3e
Sandro Duarte <sandroduarte@tre-rs.gov.br>
2012-01-16 14:53:59 -0200

    again, don't need most of the meta programming stuff. Just include the module.

--- lib/userstamper.rb
+++ lib/userstamper.rb
@@ -10,36 +10,32 @@ module Ddb
     # specific implementation details.
     module Userstamp
       def self.included(base) # :nodoc:
-        base.send           :include, InstanceMethods
         base.before_filter  :set_stamper
         base.after_filter   :reset_stamper
       end
 
-      module InstanceMethods
-        private
-          # The <tt>set_stamper</tt> method as implemented here assumes a couple
-          # of things. First, that you are using a +User+ model as the stamper
-          # and second that your controller has a <tt>current_user</tt> method
-          # that contains the currently logged in stamper. If either of these
-          # are not the case in your application you will want to manually add
-          # your own implementation of this method to the private section of
-          # the controller where you are including the Userstamp module.
-          def set_stamper
-              (Ddb::Userstamp.stamper_klass || User).stamper = self.current_user
-          end
+      private
+        # The <tt>set_stamper</tt> method as implemented here assumes a couple
+        # of things. First, that you are using a +User+ model as the stamper
+        # and second that your controller has a <tt>current_user</tt> method
+        # that contains the currently logged in stamper. If either of these
+        # are not the case in your application you will want to manually add
+        # your own implementation of this method to the private section of
+        # the controller where you are including the Userstamp module.
+        def set_stamper
+            (Ddb::Userstamp.stamper_klass || User).stamper = self.current_user
+        end
 
-          # The <tt>reset_stamper</tt> method as implemented here assumes that a
-          # +User+ model is being used as the stamper. If this is not the case then
-          # you will need to manually add your own implementation of this method to
-          # the private section of the controller where you are including the
-          # Userstamp module.
-          def reset_stamper
-             (Ddb::Userstamp.stamper_klass || User).reset_stamper
-          end
-        #end private
-      end
+        # The <tt>reset_stamper</tt> method as implemented here assumes that a
+        # +User+ model is being used as the stamper. If this is not the case then
+        # you will need to manually add your own implementation of this method to
+        # the private section of the controller where you are including the
+        # Userstamp module.
+        def reset_stamper
+           (Ddb::Userstamp.stamper_klass || User).reset_stamper
+        end
+      #end private
+
     end
   end
 end
-
-ActionController::Base.send(:include, Ddb::Controller) if defined?(ActionController)
